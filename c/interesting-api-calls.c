60286f823de98c11ebf6421fcfc75cbae3380096
raptor <raptor@0xdeadbeef.info>
2022-03-19 10:43:52 +0100

    Add interesting-api-calls

---
+++ c/interesting-api-calls.c
@@ -0,0 +1,68 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+//
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFSIZE 256
+
+int drop_priv_perm()
+{
+	// ruleid: interesting-api-calls
+	setuid(getuid());
+}
+
+int drop_priv_temp()
+{
+	// ruleid: interesting-api-calls
+	seteuid(getuid());
+}
+
+int copy_append_string(char *string1, char *string2)
+{
+	char buf[BUFSIZE];
+
+	// ruleid: interesting-api-calls
+	strcpy(buf, string1);
+
+	// ruleid: interesting-api-calls
+	strcat(buf, string2);
+}
+
+int copy_string(char *string)
+{
+	char buf[BUFSIZE];
+
+	// ruleid: interesting-api-calls
+	stpcpy(buf, string);
+}
+
+int copy_string1(char *string)
+{
+	char buf[BUFSIZE];
+
+	// ruleid: interesting-api-calls
+	strncpy(buf, string, BUFSIZE);
+}
+
+int copy_string2(char *string)
+{
+	char buf[BUFSIZE];
+
+	// ruleid: interesting-api-calls
+	stpncpy(buf, string, BUFSIZE);
+}
+
+int get_string()
+{
+	char buf[BUFSIZE];
+
+	// ruleid: interesting-api-calls
+	gets(buf);
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

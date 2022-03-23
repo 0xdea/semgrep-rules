dcf6d8878c83c2fd74abd411a7e426bfce97c911
raptor <raptor@0xdeadbeef.info>
2022-03-23 10:48:43 +0100

    Add memory-address-exposure, fix some insecure-api-*

---
+++ c/memory-address-exposure.c
@@ -0,0 +1,33 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+
+#define BUFSIZE 256
+#define FMT "whatever"
+
+int print_address(char *string)
+{
+	char buf[BUFSIZE];
+	char fmt[] = "whatever";
+
+	// ruleid: memory-address-exposure
+	printf("address: %p\n", buf);
+
+	// ruleid: memory-address-exposure
+	sprintf(buf, "address: %x\n", string);
+
+	// probably ok
+	sprintf(buf, FMT, string);
+
+	// probably ok
+	sprintf(buf, fmt, string);
+
+	// probably ok
+	sprintf(buf, string);
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

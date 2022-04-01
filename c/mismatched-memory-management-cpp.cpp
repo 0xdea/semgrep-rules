e4f6f9619b35ada91ad2517ce1e54c742e9a04ff
raptor <raptor@0xdeadbeef.info>
2022-04-01 12:15:20 +0200

    Add mismatched-memory-management

---
+++ c/mismatched-memory-management-cpp.cpp
@@ -0,0 +1,76 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bad1()
+{
+	BarObj *ptr = new BarObj()
+
+	// ruleid: mismatched-memory-management-cpp
+	free(ptr);
+}
+
+void good1()
+{
+	BarObj *ptr = new BarObj()
+
+	// ok: mismatched-memory-management-cpp
+	delete ptr;
+}
+
+class A {
+	void bad2();
+	void good2();
+};
+
+void A::bad2()
+{
+	int *ptr;
+	ptr = (int*)malloc(sizeof(int));
+
+	// ruleid: mismatched-memory-management-cpp
+	delete ptr;
+}
+
+void A::good2()
+{
+	int *ptr;
+	ptr = (int*)malloc(sizeof(int));
+
+	// ok: mismatched-memory-management-cpp
+	free(ptr);
+}
+
+class B {
+	void bad3(bool);
+	void good3();
+};
+
+void B::bad3(bool heap) {
+	int localArray[2] = { 11,22 };
+	int *p = localArray;
+
+	if (heap) {
+		p = new int[2];
+	}
+
+	// ruleid: mismatched-memory-management-cpp
+	delete[] p;
+}
+
+void B::good3() {
+	int localArray[2] = { 11,22 };
+	int *p = localArray;
+
+	p = new (std::nothrow) int[2];
+
+	// ok: mismatched-memory-management-cpp
+	delete[] p;
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}
---
+++ c/mismatched-memory-management.c
@@ -0,0 +1,28 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bad1()
+{
+	int localArray[2] = { 11, 22 };
+	int *p = localArray;
+
+	// ruleid: mismatched-memory-management
+	free(p);
+}
+
+void good1()
+{
+	int *ptr;
+	ptr = (int*)malloc(sizeof(int));
+
+	// ok: mismatched-memory-management
+	free(ptr);
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

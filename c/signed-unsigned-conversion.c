cce488ba93e159e4bc75ada8848d148abf28b5d1
raptor <raptor@0xdeadbeef.info>
2022-04-01 12:37:40 +0200

    Add code sample

--- c/signed-unsigned-conversion.c
+++ c/signed-unsigned-conversion.c
@@ -20,6 +20,50 @@ static int _php_filter_validate_domain(char * domain, int len, zend_long flags)
 	// ...
 }
 
+// https://blog.quarkslab.com/heap-overflow-in-openbsds-slaacd-via-router-advertisement.html
+char*
+parse_dnssl(char* data, int datalen)
+{
+    int len, pos;
+    char *nssl, *nsslp;
+
+    if((nssl = calloc(1, datalen + 1)) == NULL) {
+        log_warn("malloc");
+        return NULL;
+    }
+    nsslp = nssl;
+
+    pos = 0;
+
+    do {
+        len = data[pos];
+        if (len > 63 || len + pos + 1 > datalen) {
+            free(nssl);
+            log_warnx("invalid label in DNSSL");
+            return NULL;
+        }
+        if (len == 0) {
+            if (pos < datalen && data[pos + 1] != 0)
+                *nsslp++ = ' ';
+            else
+                break;
+        } else {
+            if (pos != 0 && data[pos - 1] != 0)
+                *nsslp++ = '.';
+            // ruleid: raptor-signed-unsigned-conversion
+            memcpy(nsslp, data + pos + 1, len);
+            nsslp += len;
+        }
+        pos += len + 1;
+    } while(pos < datalen);
+    if (len != 0) {
+        free(nssl);
+        log_warnx("invalid label in DNSSL");
+        return NULL;
+    }
+    return nssl;
+}
+
 // ruleid: raptor-signed-unsigned-conversion
 unsigned int readdata() 
 {

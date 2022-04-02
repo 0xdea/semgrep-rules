d35562798b70bda900339d7c90d3172d7ca6e014
raptor <raptor@0xdeadbeef.info>
2022-04-02 15:56:35 +0200

    Add code sample

--- c/insecure-api-scanf-etc.c
+++ c/insecure-api-scanf-etc.c
@@ -38,6 +38,28 @@ int test_func()
 	scanf ("%s", last_name);
 }
 
+// https://github.com/pedrib/PoC/blob/master/advisories/Pwn2Own/Tokyo_2019/tokyo_drift/tokyo_drift.md
+undefined4 sa_setBlockName(char *block_name,int len)
+{
+	int scanf_res;
+	char *__src;
+	char scanf_str [1024];
+	undefined4 scanf_int;
+
+	scanf_int = 0;
+	scanf_str._0_4_ = 0;
+	memset(scanf_str + 4,0,0x3fc);
+	print_debug(3,"%s(%d);\n","sa_setBlockName",0x42d);
+	if (len != 0) {
+		// ruleid: raptor-insecure-api-scanf-etc
+		scanf_res = sscanf(block_name,"%d%s",&scanf_int,scanf_str);
+		if ((scanf_res == 2) && (__src = strstr(block_name,s_@_000662bc), __src != NULL)) {
+			// ...
+    		}
+  	}
+	return 0;
+}
+
 int main() 
 {
 	printf("Hello, World!");

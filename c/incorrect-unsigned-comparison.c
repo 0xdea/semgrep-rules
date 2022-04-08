db4aa34df1ea3e4a2acbb77a30d641ff46d4f0e6
raptor <raptor@0xdeadbeef.info>
2022-04-08 17:57:10 +0200

    Add incorrect-unsigned-comparison

---
+++ c/incorrect-unsigned-comparison.c
@@ -0,0 +1,31 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+
+int bad1()
+{
+	size_t uvar;
+
+	// ruleid: incorrect-unsigned-comparison
+	if (uvar < 0)
+		return 1;
+
+	return 0;
+}
+
+int bad2()
+{
+	size_t uvar;
+
+	// ruleid: incorrect-unsigned-comparison
+	if (uvar <= 0)
+		return 1;
+
+	return 0;
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

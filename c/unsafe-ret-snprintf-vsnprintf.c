a008e188e54e8e0fc6b695b65598d55a7bf505d9
raptor <raptor@0xdeadbeef.info>
2022-04-08 18:30:19 +0200

    Fix unsafe-ret-*

--- c/unsafe-ret-snprintf-vsnprintf.c
+++ c/unsafe-ret-snprintf-vsnprintf.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #define BUFSIZE 256
+#define SIZE(x, y) (sizeof(x) ((y) (x)))
 
 int copy_string(char *string)
 {
@@ -13,6 +14,16 @@ int copy_string(char *string)
 	length = snprintf(buf, BUFSIZE, "%s", string);
 
 	// use length to access buf
+}
+
+int bad()
+{
+	char buf[1024], *ptr;
+	ptr = buf;
+	// ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+	ptr += snprintf(ptr, SIZE(buf, ptr), "user: %s\n", username);
+	// ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+	ptr += snprintf(ptr, SIZE(buf, ptr), "pass: %s\n", password);
 }
 
 int main() 

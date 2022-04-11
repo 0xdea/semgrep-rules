4fc63fcd251fcfab666d01e69a3dd2c8c916243d
raptor <raptor@0xdeadbeef.info>
2022-04-11 09:11:45 +0200

    Add insecure-api-atoi-atol-atof

---
+++ c/insecure-api-atoi-atol-atof.c
@@ -0,0 +1,22 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int converter_bad (const char * numstr) 
+{
+	// ruleid: raptor-insecure-api-atoi-atol-atof
+	return atoi(numstr);
+}
+
+int converter_good (const char * numstr) 
+{
+	// ok: raptor-insecure-api-atoi-atol-atof
+	return strtol(numstr, NULL, 10);
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

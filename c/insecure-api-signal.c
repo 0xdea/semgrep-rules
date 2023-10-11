960256ba23f7c6cc165fd409117de9754819237d
raptor <raptor@0xdeadbeef.info>
2023-10-11 14:46:57 +0200

    Add insecure-api-signal

---
+++ c/insecure-api-signal.c
@@ -0,0 +1,28 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+char *logMessage;
+
+void handler(int sigNum) 
+{
+	syslog(LOG_NOTICE, "%s\n", logMessage);
+	free(logMessage);
+
+	sleep(10);
+	exit(0);
+}
+
+int main(int argc, char* argv[]) 
+{
+	logMessage = strdup(argv[1]);
+
+	// ruleid: raptor-insecure-api-signal 
+	signal(SIGHUP, handler);
+	// ruleid: raptor-insecure-api-signal 
+	signal(SIGTERM, handler);
+
+	sleep(10);
+}

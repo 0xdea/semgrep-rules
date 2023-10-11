9be9129bf8c7f58be8b71e9adef6c437f6976cbd
raptor <raptor@0xdeadbeef.info>
2023-10-11 15:12:04 +0200

    Add putenv-stack-var

---
+++ c/putenv-stack-var.c
@@ -0,0 +1,37 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STR_MAX 256
+
+int putName_bad() 
+{
+	char name[STR_MAX];
+
+	fillInName(name);
+
+	// ruleid: raptor-putenv-stack-var
+	putenv(name);
+	
+	return 0;
+}
+
+int putName_good() 
+{
+	char *name = (char *)malloc(STR_MAX);
+
+	fillInName(name);
+
+	// ok: raptor-putenv-stack-var
+	putenv(name);
+
+	return 0;
+}
+
+int main() 
+{
+	printf("Hello, World!");
+	return 0;
+}

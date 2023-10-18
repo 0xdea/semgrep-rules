3b82cfc44b193d2b8fa31b6dc110561665df285f
raptor <raptor@0xdeadbeef.info>
2023-10-18 21:12:31 +0200

    Fix code sample

--- c/missing-return.c
+++ c/missing-return.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 
 // ruleid: raptor-missing-return
-int bad1()
+int bad1(void)
 {
 	printf("Hello, World!");
 }
 
 // ok: raptor-missing-return
-void good1()
+void good1(void)
 {
 	printf("Hello, World!");
 }
 
 // ok: raptor-missing-return
-int main() 
+int main(int argc, char **argv) 
 {
 	printf("Hello, World!");
 	return 0;

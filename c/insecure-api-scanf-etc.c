42875ab908958e812f399c1bc7c7b9ec4b4d2b6d
raptor <raptor@0xdeadbeef.info>
2025-05-19 12:32:58 +0200

    Fix failing unit test

--- c/insecure-api-scanf-etc.c
+++ c/insecure-api-scanf-etc.c
@@ -70,9 +70,9 @@ undefined4 sa_setBlockName(char *block_name,int len)
 	if (len != 0) {
 		// ruleid: raptor-insecure-api-scanf-etc
 		scanf_res = sscanf(block_name,"%d%s",&scanf_int,scanf_str);
-		// if ((scanf_res == 2) && (__src = strstr(block_name, s), __src != NULL)) {
+		/* if ((scanf_res == 2) && (__src = strstr(block_name, s), __src != NULL)) {
 			// ...
-    		}
+    	} */
   	}
 	return 0;
 }

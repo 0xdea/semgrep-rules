745945fc2cea9080b6f9c372de91ac9a1ff9431f
raptor <raptor@0xdeadbeef.info>
2025-11-09 00:24:50 +0100

    feat: exclude length known at compile time from memcpy matches

--- rules/c/interesting-api-calls.c
+++ rules/c/interesting-api-calls.c
@@ -61,6 +61,81 @@ void get_string()
 	gets(buf);
 }
 
+void memory_copy()
+{
+
+	// ...
+
+	// ruleid: raptor-interesting-api-calls
+	memcpy(&(((char *)p_buf)[serialization_buffer_offset]),
+		   serialized_context_buffer,
+		   serialized_context_size);
+
+	// ...
+
+	// ruleid: raptor-interesting-api-calls
+	memcpy(&(((char *)p_buf)[serialization_buffer_offset]),
+		   serialized_profiles_context_buffer,
+		   cfl_sds_len(serialized_profiles_context_buffer));
+
+	// ...
+
+	// ruleid: raptor-interesting-api-calls
+	memcpy((void *)str->wrapped.buf, src, 3 * length + 1);
+
+	// ...
+
+	if (net_setup == NULL)
+	{
+		flb_net_setup_init(&stream->net);
+	}
+	else
+	{
+		// ok: raptor-interesting-api-calls
+		memcpy(&stream->net, net_setup, sizeof(struct flb_net_setup));
+	}
+
+	// ...
+
+	crc = __bswap_32(crc32(0, (const Bytef *)data, len));
+	// ok: raptor-interesting-api-calls
+	memcpy((char *)c->block_data + outlen, &crc, 4);
+	c->used_size = outlen + 4;
+
+	// ...
+
+	// ok: raptor-interesting-api-calls
+	memcpy(&load_command, pcmd, sizeof load_command);
+
+	// ...
+
+	// chose not to use pattern-not: $FUN($_, $_, <... sizeof(...) ...>, ...)
+	// ruleid: raptor-interesting-api-calls
+	memcpy(temp, sysconfig->sortlist,
+		   sizeof(*channel->sortlist) * sysconfig->nsortlist);
+}
+
+// https://cc-sw.com/semgrep-guide-for-a-security-engineer-part-6-of-6/
+static int service_attr_req(sdp_req_t *req, sdp_buf_t *buf) {
+  sdp_cont_state_t *cstate = NULL;
+  // ...
+  if (cstate) {
+  sdp_buf_t *pCache = sdp_get_cached_rsp(cstate);
+ 
+  SDPDBG("Obtained cached rsp : %p", pCache);
+ 
+  if (pCache) {
+    short sent = MIN(max_rsp_size, pCache->data_size - cstate->cStateValue.maxBytesSent);
+    pResponse = pCache->data;
+	// ruleid: raptor-interesting-api-calls
+    memcpy(buf->data, pResponse + cstate->cStateValue.maxBytesSent, sent);  //Out-of-bound read here
+    buf->data_size += sent;
+    cstate->cStateValue.maxBytesSent += sent;
+    // ...
+  }
+  // ...
+}
+
 int main()
 {
 	printf("Hello, World!");

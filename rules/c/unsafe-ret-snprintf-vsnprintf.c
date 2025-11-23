0ba302fbea8df2246a8cc2109f76aa59c0e93544
raptor <raptor@0xdeadbeef.info>
2025-11-23 16:17:40 +0100

    doc: add a code sample

--- rules/c/unsafe-ret-snprintf-vsnprintf.c
+++ rules/c/unsafe-ret-snprintf-vsnprintf.c
@@ -26,6 +26,25 @@ void bad()
 	ptr += snprintf(ptr, SIZE(buf, ptr), "pass: %s\n", password);
 }
 
+#define PRIx64 "llx"
+
+void bad2()
+{
+	// ...
+
+	// excluded due to the format string regex used to reduce false positives
+	// todoruled: raptor-unsafe-ret-snprintf-vsnprintf
+	len = snprintf(buf, sizeof(buf) - 1,
+				   "Fluent Bit has been running: "
+				   " %i day%s, %i hour%s, %i minute%s and %i second%s",
+				   days, (days > 1) ? "s" : "", hours,
+				   (hours > 1) ? "s" : "", minutes,
+				   (minutes > 1) ? "s" : "", seconds,
+				   (seconds > 1) ? "s" : "");
+
+	// ...
+}
+
 int log(int fd, char *fmt, ...)
 {
 	char buffer[BUFSIZE];

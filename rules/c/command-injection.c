8197644a73a0deb05858f39ae044d681aac97708
raptor <raptor@0xdeadbeef.info>
2026-01-21 10:28:23 +0100

    test: add code samples for command-injection

--- rules/c/command-injection.c
+++ rules/c/command-injection.c
@@ -1,5 +1,6 @@
 // Marco Ivaldi <raptor@0xdeadbeef.info>
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -44,6 +45,55 @@ int send_mail(char *user)
 	if (fp == NULL)
 		return 1;
 	// ...
+}
+
+enum
+{
+    BUFFERSIZE = 512
+};
+
+void test_001(const char *input)
+{
+    char cmdbuf[BUFFERSIZE];
+    int len_wanted = snprintf(cmdbuf, BUFFERSIZE,
+                              "any_cmd '%s'", input);
+    if (len_wanted >= BUFFERSIZE)
+    {
+        /* Handle error */
+    }
+    else if (len_wanted < 0)
+    {
+        /* Handle error */
+    }
+    // ruleid: raptor-command-injection
+    else if (system(cmdbuf) == -1)
+    {
+        /* Handle error */
+    }
+}
+
+void test_002(const char *input)
+{
+    char cmdbuf[BUFFERSIZE];
+    int len_wanted = snprintf(cmdbuf, BUFFERSIZE,
+                              "any_cmd '%s'", input);
+    // ruleid: raptor-command-injection
+    system(cmdbuf);
+}
+
+void test_003(const char *input)
+{
+    // ok: raptor-command-injection
+    storer->store_binary(Clocks->system());
+}
+
+void test_004(const char *input)
+{
+    // ok: raptor-command-injection
+    storer->store_binary(Clocks::system());
+
+    // ok: raptor-command-injection
+    double passed_server_time = max(Clocks::system() - old_server_time, 0.0);
 }
 
 int main()

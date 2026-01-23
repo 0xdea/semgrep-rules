c6b54116cc3ee1a31dcdd966046abda265edfe03
raptor <raptor@0xdeadbeef.info>
2026-01-23 08:59:28 +0100

    test: add code samples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,7 +21,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Add detections for `syslog` and `err/warn` function families (`memory-address-exposure`).
 * Add detections for missing `signal` family functions (`insecure-api-signal`, `interesting-api-calls`).
 * Add detection for `tmpnam_r` (`insecure-api-mktemp-tmpnam-tempnam`, `interesting-api-calls`).
-* Add some code samples (`command-injection`, `format-string-bugs`, `incorrect-use-of-sizeof`, `use-of-source-size-in-copy`, `unterminated-string-strncpy`).
+* Add some code samples (`command-injection`, `format-string-bugs`, `incorrect-use-of-sizeof`, `insecure-api-access-stat`, `use-of-source-size-in-copy`, `unterminated-string-strncpy`).
 
 ### Changed
 
--- rules/c/insecure-api-access-stat.c
+++ rules/c/insecure-api-access-stat.c
@@ -56,3 +56,73 @@ int main()
 	printf("Hello, World!");
 	return 0;
 }
+
+void test_001()
+{
+	const char *original_key = "path/to/file/filename";
+	const char *mirror_key = "path/to/another/file/filename";
+
+	// ruleid: raptor-insecure-api-access-stat
+	if ((access(original_key, F_OK) == 0) && (access(mirror_key, F_OK) == 0))
+	{
+		copy_file("/bin/cp %s %s", original_key, mirror_key);
+
+		unlink(original_key);
+	}
+}
+
+void test_002()
+{
+	const char *original_key = "path/to/file/filename";
+
+	// ruleid: raptor-insecure-api-access-stat
+	if (access(original_key, W_OK) == 0)
+	{
+		FILE *fp = fopen(original_key, "wb");
+	}
+}
+
+void test_001_stack_overflow()
+{
+	struct stat buf;
+	FILE *fp;
+	char data[2560];
+
+	// ruleid: raptor-insecure-api-access-stat
+	if (stat(file.c_str(), &buf) == 0)
+	{
+
+		// Open the file for reading
+		fp = fopen(file.c_str(), "r");
+		if (fp == NULL)
+		{
+			char message[2560];
+			sprintf(message, "File '%s' Cound Not be Opened", file.c_str());
+			// DISPLAY_MSG_ERROR( this, message, "GetFileContents", "System" );
+			throw message;
+		}
+
+		// Read the file
+		MvString s, ss;
+		while (fgets(data, sizeof(data), fp) != (char *)0)
+		{
+			s = data;
+			s.trimBoth();
+			if (s.compare(0, 5, "GROUP") == 0)
+			{
+				// size_t t = s.find_last_of( ":" );
+				size_t t = s.find(":");
+				if (t != string::npos)
+				{
+					ss = s.substr(t + 1).c_str();
+					ss.trimBoth();
+					ss = ss.substr(1, ss.length() - 3).c_str();
+					group_list.push_back(ss);
+				}
+			}
+		}
+
+		// Close the file
+		fclose(fp);
+	}
+}

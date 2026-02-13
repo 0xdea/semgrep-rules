d1e45d94713a9d329f8023dfeefd54cc8314ba13
raptor <raptor@0xdeadbeef.info>
2026-02-13 15:10:14 +0100

    test: add code samples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,7 +22,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Add detections for missing `signal` family functions (`insecure-api-signal`, `interesting-api-calls`).
 * Add detection for `tmpnam_r` (`insecure-api-mktemp-tmpnam-tempnam`, `interesting-api-calls`).
 * Add detection for `VirtualAlloc` (`integer-wraparound`).
-* Add some code samples (`command-injection`, `format-string-bugs`, `incorrect-use-of-sizeof`, `insecure-api-access-stat`, `insecure-api-gets`, `insecure-api-scanf`, `insecure-api-sprintf-vsprintf`, `off-by-one`, `use-of-source-size-in-copy`, `unterminated-string-strncpy`).
+* Add some code samples (`command-injection`, `format-string-bugs`, `incorrect-use-of-sizeof`, `insecure-api-access-stat`, `insecure-api-gets`, `insecure-api-scanf`, `insecure-api-sprintf-vsprintf`, `off-by-one`, `unsafe-ret-snprintf-vsnprintf`, `use-of-source-size-in-copy`, `unterminated-string-strncpy`).
 
 ### Changed
 
--- rules/c/unsafe-ret-snprintf-vsnprintf.c
+++ rules/c/unsafe-ret-snprintf-vsnprintf.c
@@ -68,6 +68,205 @@ int log(int fd, char *fmt, ...)
 	return 0;
 }
 
+#include <iostream>
+
+int test()
+{
+	char myname[5];
+	// there is an overflow, but the source buffer is constant, so we're not really interested in this case
+	// ok: raptor-unsafe-ret-snprintf-vsnprintf
+	int ret = snprintf(myname, 10, "0123456789");
+
+	char name2[5] = "1234";
+	// ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+	int len = snprintf(0, 0, "My name is: %s", name2);
+	char *p = (char *)malloc(len);
+	snprintf(p, len, "My name is: %s", name2);
+
+	// prints 123
+	std::cout << p;
+
+	return 0;
+}
+
+char *f(const char *input) {
+    char buf[128];
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "%s", input);
+
+    char *out = malloc(len + 1);
+    // This isn't technically a vulnerability but it is suspicious:
+    // * `snprintf(...)` will not return `-1` because the format string is valid
+    // * `buf` will be NUL-terminated, because `128 > 0`
+    // * `strncpy(...)` will only copy `max(strlen(buf), len + 1)` characters
+    // * `strlen(buf)` is less than `len + 1`, so `out` will be NUL-terminated
+    strncpy(out, buf, len + 1);
+
+    // OOB read
+    memcpy(out, buf, len + 1);
+
+    return out;
+}
+
+char *non_string(const char *input, size_t n) {
+    char buf[128];
+    // ok: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "%u", n);
+
+    memcpy(out, buf, len + 1);
+
+    return out;
+}
+
+char *f(const char *input) {
+    char buf[128];
+    // there is a length check below
+    // todook: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "any_cmd '%s'", input);
+    if (len < 0 || len > 128 - 1) {
+        return NULL;
+    }
+
+    char *out = malloc(len);
+    strncpy(out, buf, len);
+    return out;
+}
+
+char *f(const char *input) {
+    char buf[128];
+    // there is a length check below
+    // todook: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "any_cmd '%s'", input);
+    if (len >= sizeof(buf)) {
+        return NULL;
+    }
+
+    char *out = malloc(len);
+    strncpy(out, buf, len);
+    return out;
+}
+
+char *f(const char *input) {
+    char buf[128];
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "any_cmd '%s'", input);
+    snprintf(buf, 128 - len, "foo");
+}
+
+char *f(const char *input) {
+    char buf[128];
+    // there is a length check below
+    // todook: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "any_cmd '%s'", input);
+    if (sizeof(buf) <= len) {
+        return NULL;
+    }
+
+    char *out = malloc(len);
+    strncpy(out, buf, len);
+    return out;
+}
+
+size_t f(const char *input) {
+    char buf[128];
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "input=%s", input);
+    return strnlen(buf, len);
+}
+
+size_t f() {
+    char buf[128];
+    // ok: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "foo");
+    return strnlen(buf, len);
+}
+
+size_t f(const char *input) {
+    char buf[128];
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    int len = snprintf(buf, 128, "input=%s", input);
+    return strncasecmp(buf, "input=foo", len);
+}
+
+void f(const char *input) {
+    char buf[128];
+    int len = 0;
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    len += snprintf(&buf[len], 128, "input=%s, input");
+    return &buf[len] - buf;
+}
+
+void wide_strings(const wchar_t *input) {
+    wchar_t buf[128];
+    int len = 0;
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    len += snprintf(&buf[len], 128, "input=%ls, input");
+    return &buf[len] - buf;
+}
+
+static char *gen_text(const char *prefix, const char *sub_prefix, char *buf, size_t sz)
+{
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    int x = snprintf(buf, sz,  "%s-%s-", prefix, sub_prefix);
+
+    // This isn't sanitized because of the `sz - 1` on the RHS
+    for ( ; (size_t)x < sz - 1; x++)
+        buf[x] = *prefix;
+
+    buf[x++] = 'x';
+    buf[x] = '\0';
+    return buf;
+}
+
+static char *
+int_to_roman(int number, const char *p)
+{
+    int len;
+    // ruleid: raptor-unsafe-ret-snprintf-vsnprintf
+    len = snprintf(numstr, sizeof(numstr), "%s", p);
+
+    for (p = numstr; *p != '\0'; p++, --len) {
+        foo();
+    }
+
+    for (p = numstr; *p != '\0'; p++, len--) {
+        foo();
+    }
+
+    // TODO: `i < len` is a sanitizer so we can't match on it being used as a
+    // loop condition
+
+    for (; i < len;) {
+        foo();
+    }
+
+    for (i = 0; i < len;) {
+        foo();
+    }
+
+    for (; i < len; i++) {
+        foo();
+    }
+
+    for (i = 0; i < len; i++) {
+        foo();
+    }
+
+    for (int i = 0; i < len;) {
+        foo();
+    }
+
+    for (int i = 0; i < len; i++) {
+        foo();
+    }
+
+    for (int i = 0, j = 0; i < len; i++) {
+        foo();
+    }
+
+    return result;
+}
+
 int main()
 {
 	printf("Hello, World!");
